0bd37505a2821354d67c0e7cc18075fcd6fa3a36
MIMU86 <43491658+MIMU86@users.noreply.github.com>
2020-05-20 16:44:27 +0800

    Add files via upload

---
+++ MainFrom/MainFrom.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace MainFrom
+{
+    partial class MainFrom
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picBox_qrcode = new System.Windows.Forms.PictureBox();
+            this.btn_verification = new System.Windows.Forms.Button();
+            this.txtcode = new System.Windows.Forms.TextBox();
+            this.lab_code = new System.Windows.Forms.Label();
+            this.lab_time = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_qrcode)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picBox_qrcode
+            // 
+            this.picBox_qrcode.Location = new System.Drawing.Point(12, 12);
+            this.picBox_qrcode.Name = "picBox_qrcode";
+            this.picBox_qrcode.Size = new System.Drawing.Size(210, 202);
+            this.picBox_qrcode.TabIndex = 0;
+            this.picBox_qrcode.TabStop = false;
+            // 
+            // btn_verification
+            // 
+            this.btn_verification.Location = new System.Drawing.Point(11, 273);
+            this.btn_verification.Name = "btn_verification";
+            this.btn_verification.Size = new System.Drawing.Size(211, 24);
+            this.btn_verification.TabIndex = 2;
+            this.btn_verification.Text = "验证";
+            this.btn_verification.UseVisualStyleBackColor = true;
+            this.btn_verification.Click += new System.EventHandler(this.btn_verification_Click);
+            // 
+            // txtcode
+            // 
+            this.txtcode.Location = new System.Drawing.Point(12, 236);
+            this.txtcode.Name = "txtcode";
+            this.txtcode.Size = new System.Drawing.Size(209, 21);
+            this.txtcode.TabIndex = 3;
+            // 
+            // lab_code
+            // 
+            this.lab_code.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_code.Location = new System.Drawing.Point(274, 126);
+            this.lab_code.Name = "lab_code";
+            this.lab_code.Size = new System.Drawing.Size(213, 40);
+            this.lab_code.TabIndex = 1;
+            this.lab_code.Text = "请验证动态码";
+            this.lab_code.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lab_time
+            // 
+            this.lab_time.Font = new System.Drawing.Font("宋体", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.lab_time.Location = new System.Drawing.Point(274, 71);
+            this.lab_time.Name = "lab_time";
+            this.lab_time.Size = new System.Drawing.Size(213, 40);
+            this.lab_time.TabIndex = 1;
+            this.lab_time.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // MainFrom
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(515, 313);
+            this.Controls.Add(this.txtcode);
+            this.Controls.Add(this.btn_verification);
+            this.Controls.Add(this.lab_time);
+            this.Controls.Add(this.lab_code);
+            this.Controls.Add(this.picBox_qrcode);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainFrom";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "谷歌动态绑定";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainFrom_FormClosing);
+            this.Load += new System.EventHandler(this.MainFrom_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.picBox_qrcode)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picBox_qrcode;
+        private System.Windows.Forms.Button btn_verification;
+        private System.Windows.Forms.TextBox txtcode;
+        private System.Windows.Forms.Label lab_code;
+        private System.Windows.Forms.Label lab_time;
+    }
+}
+
---
+++ MainFrom/MainFrom.cs
@@ -0,0 +1,97 @@
+﻿using BLL;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using ThoughtWorks.QRCode.Codec;
+
+namespace MainFrom
+{
+    public partial class MainFrom : Form
+    {
+        public MainFrom()
+        {
+            InitializeComponent();
+        }
+
+        Thread th = null;
+        private bool flas = false;
+        private string guid = "94B4442D900F49CCA3F72D1AF5BF836D";//Guid.NewGuid().ToString().Replace("-", "");
+
+        private void btn_verification_Click(object sender, EventArgs e)
+        {
+            GoogleAuthenticator authenticator = new GoogleAuthenticator(30, guid);
+            string googlecode = authenticator.GenerateCode();
+            if (googlecode.ToUpper() == txtcode.Text.Trim().ToUpper())
+            {
+                if (!flas)
+                {
+                    flas = true;
+                    th = new Thread(() =>
+                    {
+                        while (flas)
+                        {
+                            if (lab_code.InvokeRequired)
+                            {
+                                lab_code.Invoke(new Action(() => { lab_code.Text = authenticator.GenerateCode(); lab_time.Text = authenticator.EXPIRE_SECONDS.ToString(); }));
+                            }
+                            else
+                            {
+                                lab_code.Text = authenticator.GenerateCode();
+                                lab_time.Text = authenticator.EXPIRE_SECONDS.ToString();
+                            }
+                            Thread.Sleep(1000);
+                        }
+                    });
+                    th.Start();
+                }
+                MessageBox.Show("验证成功");
+
+            }
+            else MessageBox.Show("验证失败");
+        }
+
+        private void MainFrom_Load(object sender, EventArgs e)
+        {
+            CreateQrCode();
+        }
+
+        #region 生成二维码
+        private void CreateQrCode()
+        {
+            string websitetitle = UrlEncode("MIMU86");
+            QRCodeCreate qcc = new QRCodeCreate();
+            // guid = Guid.NewGuid().ToString().Replace("-", "");
+            string gcode = Base32.ToString(Encoding.UTF8.GetBytes(guid)).Replace("=", "");
+            string content = string.Format("otpauth://totp/{0}?secret={1}&issuer=", websitetitle, gcode);
+            picBox_qrcode.Image = qcc.CreateQRCode(content, QRCodeEncoder.ENCODE_MODE.BYTE, QRCodeEncoder.ERROR_CORRECTION.M, 0, 5, 0, 0);
+        }
+        #endregion
+
+        #region Url编码
+        public static string UrlEncode(string str)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] byStr = System.Text.Encoding.UTF8.GetBytes(str); //默认是System.Text.Encoding.Default.GetBytes(str)
+            for (int i = 0; i < byStr.Length; i++)
+            {
+                sb.Append(@"%" + Convert.ToString(byStr[i], 16));
+            }
+
+            return (sb.ToString());
+        }
+        #endregion
+
+
+        private void MainFrom_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            flas = false;
+        }
+
+    }
+}
